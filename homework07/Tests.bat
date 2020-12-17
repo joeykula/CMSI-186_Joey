d0f0005c28c540aa5b2a54842b4f1c3ca612d313
joeykula <70680092+joeykula@users.noreply.github.com>
2020-12-16 16:54:20 -0800

    Update Tests.bat

--- homework07/Tests.bat
+++ homework07/Tests.bat
@@ -1,71 +1,71 @@
 @del TestResults.txt
-@del BirthdaySolver.class
+@del .class
 @rmdir /Q /S docs
 
 @mkdir docs
-@javadoc -d docs BirthdaySolver.java
+@javadoc -d docs Birthday_solver.java
 @javac BirthdaySolver.java
 
 @echo Creating TestResults file...
 echo Test Results >> TestResults.txt
 
-@echo Testing jibberish...
-echo Running BirthdaySolver with asdf and laskdfh as inputs... >> TestResults.txt
-java BirthdaySolver asdf laskdfa >> TestResults.txt
+@echo Testing strings...
+echo Running Birthday_solver with heeheehee and hoohoohoo as inputs... >> TestResults.txt
+java Birthday_solver heeheehee hoohoohoo >> TestResults.txt
 echo . >> TestResults.txt
 
 @echo Testing very large values...
-echo Running BirthdaySolver with 500 and 100000000 as inputs... >> TestResults.txt
-java BirthdaySolver 500 100000000 >> TestResults.txt
+echo Running Birthday_solver with 500 and 100000000 as inputs... >> TestResults.txt
+java Birthday_solver 500 100000000 >> TestResults.txt
 echo . >> TestResults.txt
 
-@echo Testing a myriad of different values...
-echo Running BirthdaySolver with 5 and 100... >> TestResults.txt
-java BirthdaySolver 5 100 >> TestResults.txt
-echo Running BirthdaySolver with 5 and 1000... >> TestResults.txt
-java BirthdaySolver 5 1000 >> TestResults.txt
-echo Running BirthdaySolver with 5 and 100000... >> TestResults.txt
-java BirthdaySolver 5 10000 >> TestResults.txt
-echo Running BirthdaySolver with 5 and 5000000... >> TestResults.txt
-java BirthdaySolver 5 5000000 >> TestResults.txt
+@echo Testing different values...
+echo Running Birthday_solver with 2 and 100... >> TestResults.txt
+java Birthday_solver 2 100 >> TestResults.txt
+echo Running Birthday_solver with 2 and 1000... >> TestResults.txt
+java Birthday_solver 2 1000 >> TestResults.txt
+echo Running Birthday_solver with 2 and 100000... >> TestResults.txt
+java Birthday_solver 2 10000 >> TestResults.txt
+echo Running Birthday_solver with 2 and 5000000... >> TestResults.txt
+java Birthday_solver 2 5000000 >> TestResults.txt
 echo . >> TestResults.txt
 
-echo Running BirthdaySolver with 10 and 100... >> TestResults.txt
-java BirthdaySolver 10 100 >> TestResults.txt
-echo Running BirthdaySolver with 10 and 1000... >> TestResults.txt
-java BirthdaySolver 10 1000 >> TestResults.txt
-echo Running BirthdaySolver with 10 and 100000... >> TestResults.txt
-java BirthdaySolver 10 10000 >> TestResults.txt
-echo Running BirthdaySolver with 10 and 5000000... >> TestResults.txt
-java BirthdaySolver 10 5000000 >> TestResults.txt
+echo Running Birthday_solver with 10 and 100... >> TestResults.txt
+java Birthday_solver 10 100 >> TestResults.txt
+echo Running Birthday_solver with 10 and 1000... >> TestResults.txt
+java Birthday_solver 10 1000 >> TestResults.txt
+echo Running Birthday_solver with 10 and 100000... >> TestResults.txt
+java Birthday_solver 10 10000 >> TestResults.txt
+echo Running Birthday_solver with 10 and 5000000... >> TestResults.txt
+java Birthday_solver 10 5000000 >> TestResults.txt
 echo . >> TestResults.txt
 
-echo Running BirthdaySolver with 30 and 100... >> TestResults.txt
-java BirthdaySolver 30 100 >> TestResults.txt
-echo Running BirthdaySolver with 30 and 1000... >> TestResults.txt
-java BirthdaySolver 30 1000 >> TestResults.txt
-echo Running BirthdaySolver with 30 and 100000... >> TestResults.txt
-java BirthdaySolver 30 100000 >> TestResults.txt
-echo Running BirthdaySolver with 30 and 5000000... >> TestResults.txt
-java BirthdaySolver 30 5000000 >> TestResults.txt
+echo Running Birthday_solver with 65 and 100... >> TestResults.txt
+java Birthday_solver 65 100 >> TestResults.txt
+echo Running Birthday_solver with 30 and 1000... >> TestResults.txt
+java Birthday_solver 65 1000 >> TestResults.txt
+echo Running Birthday_solver with 30 and 100000... >> TestResults.txt
+java Birthday_solver 65 100000 >> TestResults.txt
+echo Running Birthday_solver with 30 and 5000000... >> TestResults.txt
+java Birthday_solver 65 5000000 >> TestResults.txt
 echo . >> TestResults.txt
 
-echo Running BirthdaySolver with 60 and 100... >> TestResults.txt
-java BirthdaySolver 60 100 >> TestResults.txt
-echo Running BirthdaySolver with 60 and 1000... >> TestResults.txt
-java BirthdaySolver 60 1000 >> TestResults.txt
-echo Running BirthdaySolver with 60 and 100000... >> TestResults.txt
-java BirthdaySolver 60 100000 >> TestResults.txt
-echo Running BirthdaySolver with 60 and 5000000... >> TestResults.txt
-java BirthdaySolver 60 5000000 >> TestResults.txt
+echo Running Birthday_solver with 81 and 100... >> TestResults.txt
+java Birthday_solver 81 100 >> TestResults.txt
+echo Running Birthday_solver with 81 and 1000... >> TestResults.txt
+java Birthday_solver 81 1000 >> TestResults.txt
+echo Running Birthday_solver with 81 and 100000... >> TestResults.txt
+java Birthday_solver 81 100000 >> TestResults.txt
+echo Running Birthday_solver with 81 and 5000000... >> TestResults.txt
+java Birthday_solver 81 5000000 >> TestResults.txt
 echo . >> TestResults.txt
 
-echo Running BirthdaySolver with 100 and 100... >> TestResults.txt
-java BirthdaySolver 100 100 >> TestResults.txt
-echo Running BirthdaySolver with 100 and 1000... >> TestResults.txt
-java BirthdaySolver 100 1000 >> TestResults.txt
-echo Running BirthdaySolver with 100 and 100000... >> TestResults.txt
-java BirthdaySolver 100 100000 >> TestResults.txt
-echo Running BirthdaySolver with 100 and 5000000... >> TestResults.txt
-java BirthdaySolver 100 5000000 >> TestResults.txt
+echo Running Birthday_solver with 111 and 100... >> TestResults.txt
+java Birthday_solver 111 100 >> TestResults.txt
+echo Running Birthday_solver with 111 and 1000... >> TestResults.txt
+java Birthday_solver 111 1000 >> TestResults.txt
+echo Running Birthday_solver with 111 and 100000... >> TestResults.txt
+java Birthday_solver 111 100000 >> TestResults.txt
+echo Running Birthday_solver with 111 and 5000000... >> TestResults.txt
+java Birthday_solver 111 5000000 >> TestResults.txt
 echo . >> TestResults.txt
